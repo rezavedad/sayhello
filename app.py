a98890aeec1248c96f19f9f95a88cbd28c75d728
vedadrez <reza.hv@outlokk.de>
2022-03-12 09:57:15 +0100

    sayhello added.

--- app.py
+++ app.py
@@ -1,16 +1,15 @@
 from flask import Flask, render_template, request, flash
+from flask import Flask, render_template, request, flash
 
 app = Flask(__name__)
-app.secret_key = "rh12345678"
+app.secret_key = "manbearpig_MUDMAN888"
+
 @app.route("/hello")
 def index():
-    flash("What is your name? ")
-    return render_template("index.html")
+	flash("what's your name?")
+	return render_template("index.html")
 
-@app.route("/greet", methods=["POST", "GET"])
-def greet():
-    flash("Hi " + (request.form['name_input']) + ", great to see you.")
-    return render_template("index.html")
-
-if __name__ == "__main__" :
-    app.run(debug=True, host="127.0.0.1")
+@app.route("/greet", methods=['POST', 'GET'])
+def greeter():
+	flash("Hi " + str(request.form['name_input']) + ", great to see you!")
+	return render_template("index.html")
